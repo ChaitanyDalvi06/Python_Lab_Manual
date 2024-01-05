6e7e9267dbd47c2cf1e520ce556deaa2b399ca58
ChaitanyDalvi06 <142775927+ChaitanyDalvi06@users.noreply.github.com>
2024-01-05 20:59:58 +0530

    Add files via upload

---
+++ 5.3_program/main.py
@@ -0,0 +1,9 @@
+from cars import BMW, AUDI, NISSAN
+
+bmw_car = BMW.BMW(model="X5", color="White")
+audi_car = AUDI.AUDI(model="A4", color="Black")
+nissan_car = NISSAN.NISSAN(model="Altima", color="Navy blue")
+
+bmw_car.display_info()
+audi_car.display_info()
+nissan_car.display_info()
